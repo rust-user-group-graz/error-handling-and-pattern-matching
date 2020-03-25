ab67e929f185189435432111f042761ab4425647
davidf <dforsten@gmail.com>
2020-03-25 17:18:30 +0100

    Added example for error propagation

---
+++ rust/simple-match-propagate/src/main.rs
@@ -0,0 +1,28 @@
+fn not_ready() -> Result<u32, String> {
+    Err("Wait for 7.5 million years!".into())
+}
+
+fn universal_answer() -> Result<u32, String> {
+    Ok(42)
+}
+
+fn do_ask_propagate_error(answer: Result<u32, String> ) -> Result<(), String> {
+    match answer {
+        Ok(i) => {
+            println!("Success - {} is the answer!", i);
+            Ok(())
+        },
+        Err(s) => Err(s)
+    }
+}
+
+fn check_answer(answer: Result<u32, String>) {
+    if let Err(s) = do_ask_propagate_error(answer) {
+        println!("Error: {}", s);
+    }
+}
+
+fn main() {
+    check_answer(not_ready());
+    check_answer(universal_answer());
+}
