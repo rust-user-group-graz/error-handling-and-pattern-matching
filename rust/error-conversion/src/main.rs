0dd75258c6bf9ec821e3c3eaf1871fa75c8c554e
davidf <dforsten@gmail.com>
2020-03-25 17:39:00 +0100

    Added an example for error conversion using map_err

---
+++ rust/error-conversion/src/main.rs
@@ -0,0 +1,27 @@
+fn not_ready() -> Result<u32, String> {
+    Err("Wait for 7.5 million years!".into())
+}
+
+fn universal_answer() -> Result<u32, String> {
+    Ok(42)
+}
+
+fn do_ask_propagate_error(answer: Result<u32, String> ) -> Result<(), AskError> {
+    println!("Success - {} is the answer!", answer.map_err(|_| AskError::NeedToWait)?);
+    Ok(())
+}
+
+enum AskError {
+    NeedToWait
+}
+
+fn check_answer(answer: Result<u32, String>) {
+    if let Err(AskError::NeedToWait) = do_ask_propagate_error(answer) {
+        println!("Error: Need to wait longer!");
+    }
+}
+
+fn main() {
+    check_answer(not_ready());
+    check_answer(universal_answer());
+}
