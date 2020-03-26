27ec5bdbd607cf4794dd4fce243eaf5d7fd142f4
davidf <dforsten@gmail.com>
2020-03-26 20:50:54 +0100

    Added an example replacing the ? operator with an equivalent match construct

---
+++ rust/simple-match-propagate-shortcut-expanded/src/main.rs
@@ -0,0 +1,28 @@
+fn not_ready() -> Result<u32, String> {
+    Err("Wait for 7.5 million years!".into())
+}
+
+fn universal_answer() -> Result<u32, String> {
+    Ok(42)
+}
+
+fn do_ask_propagate_error(answer: Result<u32, String> ) -> Result<(), String> {
+    println!("Success - {} is the answer!", {
+        match answer {
+            Ok(i) => i,
+            Err(s) => return Err(s)
+        }
+    });
+    Ok(())
+}
+
+fn check_answer(answer: Result<u32, String>) {
+    if let Err(s) = do_ask_propagate_error(answer) {
+        println!("Error: {}", s);
+    }
+}
+
+fn main() {
+    check_answer(not_ready());
+    check_answer(universal_answer());
+}
